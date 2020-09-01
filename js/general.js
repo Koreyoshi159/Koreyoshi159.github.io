3d1209c32fa7f56e5d242fc0d23a535b4f2264d2
Hao Guo <guo0012@hz.nl>
2020-09-01 11:22:25 +0200

    new content

--- js/general.js
+++ js/general.js
@@ -1,25 +1,3 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-    // hide();
 });
-
-// function hide(){
-//     $(".profile, .dashboard, .faq").hide(1000);
-//     $('#home').click(function(){
-//         $('.home').show();
-//         $(".profile, .dashboard, .faq").hide(1000);
-//     });
-//     $('#profile').click(function(){
-//         $('.profile').show();
-//         $(".home, .dashboard, .faq").hide(1000);
-//     });
-//     $('#dashboard').click(function(){
-//         $('.dashboard').show();
-//         $(".home, .profile, .faq").hide(1000);
-//     });
-//     $('#faq').click(function(){
-//         $('.faq').show();
-//         $(".home, .profile, .dashboard").hide(1000);
-//     });
-
-// }
