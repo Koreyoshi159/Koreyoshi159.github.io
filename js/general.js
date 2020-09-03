09b1268c03a6b8568af0fbd8d46fdd4e41e75a14
Hao Guo <guo0012@hz.nl>
2020-09-03 15:54:13 +0200

    new changes

--- js/general.js
+++ js/general.js
@@ -1,3 +1,63 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+    console.log("ready!");
+    printTable();
 });
+
+let course = [
+    "Opleidings- en beroepsorientatie",
+    "Computer Science Basics",
+    "Programming basics",
+    "Professional skills 1",
+    "HZ personality 1",
+    "Object-oriented programming",
+    "Professional skills 2",
+    "Professionele werkplek",
+    "Framework development 1",
+    "Framework project 1",
+    "Professional skills 3",
+    "IT personality 1",
+    "Framework development 2",
+    "Framework project 2"
+];
+
+let ec = [
+    2.5,
+    7.5,
+    5,
+    2.5,
+    2.5,
+    10,
+    2.5,
+    2.5,
+    5,
+    5,
+    2.5,
+    2.5,
+    5,
+    5
+];
+
+let grade = [
+    "NULL",
+    "NULL",
+    "NULL",
+    "NULL",
+    "NULL",
+    "NULL",
+    "NULL",
+    "NULL",
+    "NULL",
+    "NULL",
+    "NULL",
+    "NULL",
+    "NULL",
+    "NULL"
+];
+
+function printTable() {
+    for (i = 0; i < course.length; ++i) {
+        $("#grade").append(
+            "<tr><td>" + course[i] + "</td><td>" + ec[i] + "</td><td>" + grade[i] + "</td></tr>"
+        );
+    };
+};
