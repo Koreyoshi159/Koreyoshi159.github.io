2db7df00312314f0606cdb868bddbe360149353e
Hao Guo <guo0012@hz.nl>
2020-10-22 23:33:56 +0200

    updated motivation page

--- js/general.js
+++ js/general.js
@@ -1,10 +1,17 @@
 $(document).ready(function () {
     console.log("ready!");
-    fadeBackground()
+    fadeBackground();
+    careerImageToggle();
 });
 
 function fadeBackground(){
     $(window).scroll(function() {
         $(".top").css("opacity", 1 - $(window).scrollTop() / 350);
       });
+}
+
+function careerImageToggle() {
+    $("#showResultImageCareer").on("click", () => {
+        $(".motivation-item-3").toggle();
+    })
 }
